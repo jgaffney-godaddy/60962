2028b96dd911cd58da4316a58691577ebde42944
John Gaffney <john.gaffney@pagely.com>
2025-03-18 09:24:18 -0700

    changes in 2024

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
  *
  * @var bool
  */
-define( 'WP_USE_THEMES', true );
+define('WP_USE_THEMES', true);
+
+print_r(['index', 'page']);
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
